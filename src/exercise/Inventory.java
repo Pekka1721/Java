6e561a680b841d825cafcb559dcc2687a4c2679a
Rajender Reddy <pekkarange1721@gmail.com>
2022-08-20 22:05:26 +0530

    chapter 5

---
+++ src/exercise/Inventory.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class Inventory {
+/* In inventory management, the economic order quantity for a single product is given by 
+ * EOQ = Squareroot(2*demand rate *setup costs/holding cost per unit time
+ * and the optimal time between order
+ * TBO = Squareroot(2*setupcosts/demand rate *holding cost per item per unit time
+ * Write a prog. to computer EOQ and TBO. given demand rate (items per unit time)setup cost(perorder) and the holding costs(per order) and the holding cost (per item per unit time).
+ * 
+ * */
+}
