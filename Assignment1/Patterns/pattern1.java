062aa5e780dd297224e871ce0d2d8e6e03e9e269
Rajender Reddy <pekkarange1721@gmail.com>
2022-09-27 12:23:05 +0530

    Patterns code

--- Assignment1/Patterns/pattern1.java
+++ Assignment1/Patterns/pattern1.java
@@ -2,15 +2,24 @@ package Patterns;
 
 public class pattern1 {
     public static void main(String[] args){
-            printPatter(5);
+            printPatter1(5);
+            System.out.println();
+            pattern2(5);
     }
-    public static void printPatter(int num){
-        for(int rows =1;rows<=num;rows++){
-            
-                for(int col = 1;col<=rows;col++){
+    public static void printPatter1(int num){
+        for(int rows =1;rows<=num;rows++){          //for the no of rows to be printing
+                for(int col = 1;col<=rows;col++){       //for no of colomms to prnt;
                     System.out.print("^ ");
                 }
             System.out.println();
         }
+    }
+    public static void pattern2(int num){
+        for(int rows =1;rows<=num;rows++){          //for the no of rows to be printing
+            for(int col = 1;col<=num;col++){       //for no of colomms to prnt;
+                System.out.print("^ ");
+            }
+        System.out.println();
+    }
     }
 }
